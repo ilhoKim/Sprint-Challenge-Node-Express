9cc6a93a7b7d1321bc387deb0e746ee1227991b0
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-05-04 12:32:19 -0700

    ActionRoutes.js, ProjectRoutes.js created / boilerplated

---
+++ Routes/ActionRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const projectModel = require('../data/helpers/actionModel.js');
+
+
+module.exports = router;
---
+++ Routes/ProjectRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const projectModel = require('../data/helpers/projectModel.js');
+
+
+module.exports = router;
