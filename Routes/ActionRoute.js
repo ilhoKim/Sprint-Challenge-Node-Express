c7446be7d2731f2b91e52447b104d5538d749677
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-03 16:39:08 -0700

    fix: fixed router path
     - fixed router path from ActionRoute.js
     - GET works at '/api/actions/'

--- Routes/ActionRoute.js
+++ Routes/ActionRoute.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../data/helpers/actionModel');
+const db = require('../data/helpers/actionModel.js');
 
 const sendUserError = (status, message, res) => {
     res
       .status(status)
       .json({errorMessage: message})
   };
+
+// GET
+router.get('/', (req, res) => {
+    db.get()
+    .then(response => {
+        res.json(response)
+    })
+    .catch(error => {
+        sendUserError(500, 'Data cannot be retrieve', res)
+        return;
+    })
+})
+
+// INSERT
+// UPDATE
+// REMOVE
 
 
 module.exports = router;
