5f0e76461c453256a0225b6af440ac6c7fd6f539
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-09-03 19:35:13 -0700

    feat: MVP complete  - code auto formatting

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require('express');
+const express = require("express");
 
-const ActionRoute = require('./Routes/ActionRoute');
-const ProjectRoute = require('./Routes/ProjectRoute');
+const ActionRoute = require("./Routes/ActionRoute");
+const ProjectRoute = require("./Routes/ProjectRoute");
 
 const server = express();
 
 const port = 5555;
 
-
 server.use(express.json());
 
-server.use('/api/actions', ActionRoute);
-server.use('/api/projects', ProjectRoute);
+server.use("/api/actions", ActionRoute);
+server.use("/api/projects", ProjectRoute);
 
-server.get('/', (req, res) => {
-    res.send('Welcom to Express');
-  });
-    
-server.listen(port, () => console.log(`\n== Express server online at port: ${port} ==\n`));
+server.get("/", (req, res) => {
+  res.send("Welcom to Express");
+});
+
+server.listen(port, () =>
+  console.log(`\n== Express server online at port: ${port} ==\n`)
+);
